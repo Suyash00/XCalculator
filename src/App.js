c7da81253f927ea126434d99411f7859fbd0f790
Suyash00 <suyashhandy@gmail.com>
2023-12-29 16:47:12 +0530

    calculator deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleEvaluate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h2>React Calculator</h2>
+      <input type="text" value={input} readOnly />
+      <div className="result">{result}</div>
+      <br />
+      <div className="buttons">
+        {[7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"].map(
+          (button, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                if (button === "C") {
+                  handleClear();
+                } else if (button === "=") {
+                  handleEvaluate();
+                } else {
+                  handleButtonClick(button);
+                }
+              }}
+            >
+              {button}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 }
